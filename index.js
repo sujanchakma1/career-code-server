8b8f16ee8e566ce1967f7303088ea88d7c658da1
sujanchakma1 <sujanckz926@gmail.com>
2025-05-30 11:58:09 +0600

    added career code server

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express()
+require('dotenv').config()
+const cors = require('cors')
+const port = process.env.PORT || 3000
+
+
+app.use(express.json())
+app.use(cors())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ra0uaai.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    const jobsCollection = client.db('careerCode').collection('jobs')
+
+    app.get('/jobs',async(req,res)=>{
+       const cursor = jobsCollection.find()
+       const result = await cursor.toArray()
+       res.send(result)
+    })
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req,res)=>{
+  res.send("Career code server running hot")
+})
+
+app.listen(port,()=>{
+  console.log(`career code running on port: ${port}`)
+})
