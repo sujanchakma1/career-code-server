ddcb74bdc21ab1d274f001d13a00bce586ff9d9f
sujanchakma1 <sujanckz926@gmail.com>
2025-06-02 16:14:45 +0600

    added my application & job

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 require('dotenv').config()
 const cors = require('cors')
@@ -27,10 +27,42 @@ async function run() {
     // await client.connect();
 
     const jobsCollection = client.db('careerCode').collection('jobs')
+    const applicationCollection = client.db('careerCode').collection('application')
 
     app.get('/jobs',async(req,res)=>{
        const cursor = jobsCollection.find()
        const result = await cursor.toArray()
+       res.send(result)
+    })
+    app.get('/jobDetails/:id', async(req,res)=>{
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await jobsCollection.findOne(query)
+      res.send(result)
+    })
+    app.post('/jobApplication/:id', async(req,res)=>{
+      const application = req.body
+      const result = await applicationCollection.insertOne(application)
+      res.send(result)
+    })
+    app.get("/myApplication",async(req,res)=>{
+      const email = req.query.email
+      const query = {
+        applicant: email
+      }
+      const result = await applicationCollection.find(query).toArray()
+     
+      for(const application of result){
+        const jobId = application.jobId
+        const jobQuery = {_id: new ObjectId (jobId)}
+        const job = await jobsCollection.findOne(jobQuery)
+        application.company = job.company 
+        application.title = job.title
+        application.company_logo = job.company_logo
+        application.jobType = job.jobType
+        application.location = job.location
+        application.salaryRange = job.salaryRange
+      }
        res.send(result)
     })
     // Send a ping to confirm a successful connection
